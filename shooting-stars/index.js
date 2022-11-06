6e782b94fb0d4b3e333c670b2134d7139f2b6480
Suraj <wijewickramasuraj@gmail.com>
2022-11-06 11:54:02 +0530

    ad

--- shooting-stars/index.js
+++ shooting-stars/index.js
@@ -149,7 +149,7 @@ export default () => {
       audioLoader.load(url, accept, function onprogress() {}, reject);
     });
 
-    // createAsteroidField(group, soundBuffer);
+    createAsteroidField(group, soundBuffer);
     app.updateMatrixWorld();
   })();
 
