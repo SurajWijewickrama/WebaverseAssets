72c86baebc0ffbf64f7755401eeb8ce81240c9b8
Suraj <wijewickramasuraj@gmail.com>
2022-11-07 09:30:34 +0530

    gltf

--- shooting-stars/index.js
+++ shooting-stars/index.js
@@ -136,7 +136,7 @@ export default () => {
     gltf = await new Promise((accept, reject) => {
       const { gltfLoader } = useLoaders();
       const url =
-        "https://SurajWijewickrama.github.io/WebaverseAssets/glow-stars.glb";
+        "https://SurajWijewickrama.github.io/WebaverseAssets/glow-stars.glft";
       gltfLoader.load(url, accept, function onprogress() {}, reject);
     });
 
